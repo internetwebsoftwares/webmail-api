2e9e8a11a079c93d6f3b85e1bc80d7f06684f5e6
internetwebdoftwares <atasteven49@gmail.com>
2021-08-13 20:05:54 +0530

    Added datalist element

--- Models/userModel.js
+++ Models/userModel.js
@@ -20,7 +20,10 @@ const userSchema = new mongoose.Schema(
       required: true,
       minLength: 7,
     },
-    clientEmailIds: [],
+    clientEmailIds: {
+      type: Array,
+      default: [],
+    },
     tokens: [
       {
         token: {
